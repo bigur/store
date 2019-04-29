3cb12b9e1a652fc800fa0ee4afdbcc748b1c6f12
Gennady Kovalev <gik@bigur.ru>
2019-04-29 21:39:29 +0300

    Bump version number

--- bigur/store/version.py
+++ bigur/store/version.py
@@ -2,4 +2,4 @@ __author__ = 'Gennady Kovalev <gik@bigur.ru>'
 __copyright__ = '(c) 2016-2018 Business group for development management'
 __licence__ = 'For license information see LICENSE'
 
-__version__ = '3.1.3'
+__version__ = '3.1.4'
